91d400ca9d882af14d6362eca607d025519b5282
Sumiara5365 <rafiq15-5365@diu.edu.bd>
2025-02-23 12:09:59 +0600

    task 5

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         drawer: Drawer(
-          child: Text("Menu01"),
+          child: ListView(children: [
+            Container(
+              height: 70,
+              child: DrawerHeader(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.tiktok),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Icon(Icons.close),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {},
+            )
+          ]),
         ),
+        endDrawer: Drawer(),
         appBar: AppBar(
           title: Text("Advanced UI-Lab06"),
           backgroundColor: Colors.blue,
